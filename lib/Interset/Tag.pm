d6d94328431bc15c36cadad76a1b1c840153e60e
Dan Zeman <zeman@ufal.mff.cuni.cz>
2012-04-24 07:53:09 +0000

    Seznam známých rysů.

--- lib/Interset/Tag.pm
+++ lib/Interset/Tag.pm
@@ -11,6 +11,45 @@ use Moose;
 
 
 
+has pos          => ( is => 'rw' );
+has subpos       => ( is => 'rw' );
+has prontype     => ( is => 'rw' );
+has numtype      => ( is => 'rw' );
+has numform      => ( is => 'rw' );
+has numvalue     => ( is => 'rw' );
+has advtype      => ( is => 'rw' );
+has punctype     => ( is => 'rw' );
+has puncside     => ( is => 'rw' );
+has synpos       => ( is => 'rw' );
+has poss         => ( is => 'rw' );
+has reflex       => ( is => 'rw' );
+has negativeness => ( is => 'rw' );
+has definiteness => ( is => 'rw' );
+has gender       => ( is => 'rw' );
+has animateness  => ( is => 'rw' );
+has number       => ( is => 'rw' );
+has case         => ( is => 'rw' );
+has prepcase     => ( is => 'rw' );
+has degree       => ( is => 'rw' );
+has subcat       => ( is => 'rw' );
+has verbform     => ( is => 'rw' );
+has mood         => ( is => 'rw' );
+has tense        => ( is => 'rw' );
+has subtense     => ( is => 'rw' );
+has voice        => ( is => 'rw' );
+has aspect       => ( is => 'rw' );
+has foreign      => ( is => 'rw' );
+has abbr         => ( is => 'rw' );
+has hyph         => ( is => 'rw' );
+has echo         => ( is => 'rw' );
+has style        => ( is => 'rw' );
+has typo         => ( is => 'rw' );
+has variant      => ( is => 'rw' );
+has tagset       => ( is => 'rw' );
+has other        => ( is => 'rw' );
+
+
+
 1;
 
 =over
