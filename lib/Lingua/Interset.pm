cdd13115cb52c88f5d78885b751979c28ec613b5
Dan Zeman <zeman@ufal.mff.cuni.cz>
2014-06-17 10:37:44 +0200

    Rearranging functions. They appear in the same order in documentation.

--- lib/Lingua/Interset.pm
+++ lib/Lingua/Interset.pm
@@ -28,171 +28,6 @@ my $driver_hash = undef; # indexed by tagset id
 
 
 #------------------------------------------------------------------------------
-# Tries to enumerate existing tagset drivers. Searches for relevant folders in
-# @INC paths.
-#------------------------------------------------------------------------------
-=func find_drivers()
-
-  my $list_of_drivers = find_drivers ();
-
-This function searches relevant folders in C<@INC> for installed Interset
-drivers for tagsets.
-It looks both for the new Interset 2 drivers (e.g. C<Lingua::Interset::EN::Penn>)
-and for the old Interset 1 drivers (e.g. C<tagset::en::penn>).
-It returns a reference to an array of hash references.
-Every hash in the list contains the following fields
-(here with example values):
-
-  my %record =
-  (
-      'old'     => 1, # 1 or 0 ... old or new driver?
-      'tagset'  => 'en::penn', # tagset id
-      'package' => 'Lingua::Interset::EN::Penn', # this is what you 'use' or 'require' in your code
-      'path'    => '/home/zeman/perl5/lib/Lingua/Interset/EN/Penn.pm' # path where it is installed
-  );
-
-Note that you may find more than one package for the same tagset id.
-This function will list all of them.
-When you ask Interset to do something with a tagset (e.g. C<decode ('en::penn', $tag)>),
-Interset will select one of the available packages for you.
-It will prefer new drivers over the old ones.
-If you have two old or two new drivers, their priority will be decided by Perl
-and it should correspond to the order of your C<$PERL5LIB> environment variable.
-To avoid confusion, it is recommended that you have each package installed only once.
-
-=cut
-sub find_drivers
-{
-    if(!defined($driver_list))
-    {
-        $driver_list = _find_drivers();
-    }
-    return $driver_list;
-}
-sub _find_drivers
-{
-    my @drivers;
-    foreach my $path (@INC)
-    {
-        # Old drivers (Interset 1.0) are in the "tagset" folder.
-        # We will continue using them until all have been ported to Interset 2.0.
-        my $tpath = "$path/tagset";
-        if(-d $tpath)
-        {
-            opendir(DIR, $tpath) or confess("Cannot read folder $tpath: $!\n");
-            my @subdirs = readdir(DIR);
-            closedir(DIR);
-            foreach my $sd (@subdirs)
-            {
-                my $sdpath = "$tpath/$sd";
-                if(-d $sdpath && $sd !~ m/^\.\.?$/)
-                {
-                    opendir(DIR, $sdpath) or confess("Cannot read folder $sdpath: $!\n");
-                    my @files = readdir(DIR);
-                    closedir(DIR);
-                    foreach my $file (@files)
-                    {
-                        my $fpath = "$sdpath/$file";
-                        my $driver = $file;
-                        if(-f $fpath && $driver =~ s/\.pm$//)
-                        {
-                            $driver = $sd."::".$driver;
-                            my %record =
-                            (
-                                'old'     => 1,
-                                'tagset'  => $driver,
-                                'package' => "tagset::$driver",
-                                'path'    => $fpath
-                            );
-                            push(@drivers, \%record);
-                        }
-                    }
-                }
-            }
-        }
-        # New drivers (Interset 2.0) are in the "Lingua/Interset" folder.
-        # Not everything in this folder is a driver! But subfolders lead to drivers, the additional stuff are files, not folders.
-        my $lipath = "$path/Lingua/Interset";
-        if(-d $lipath)
-        {
-            opendir(DIR, $lipath) or confess("Cannot read folder $lipath: $!\n");
-            my @subdirs = readdir(DIR);
-            closedir(DIR);
-            foreach my $sd (@subdirs)
-            {
-                my $sdpath = "$lipath/$sd";
-                if(-d $sdpath && $sd !~ m/^\.\.?$/)
-                {
-                    opendir(DIR, $sdpath) or confess("Cannot read folder $sdpath: $!\n");
-                    my @files = readdir(DIR);
-                    closedir(DIR);
-                    foreach my $file (@files)
-                    {
-                        my $fpath = "$sdpath/$file";
-                        my $driver = $file;
-                        if(-f $fpath && $driver =~ s/\.pm$//)
-                        {
-                            my $driver_uppercased = $sd.'::'.$driver;
-                            my $driver_lowercased = lc($driver_uppercased);
-                            my %record =
-                            (
-                                'old'     => 0,
-                                'tagset'  => $driver_lowercased,
-                                'package' => "Lingua::Interset::$driver_uppercased",
-                                'path'    => $fpath
-                            );
-                            push(@drivers, \%record);
-                        }
-                    }
-                }
-            }
-        }
-    }
-    @drivers = sort {$a->{tagset} cmp $b->{tagset}} (@drivers);
-    return \@drivers;
-}
-
-
-
-#------------------------------------------------------------------------------
-# Returns the set of all known tagset drivers indexed by tagset id. If there
-# are two drivers installed for the same tagset, it is not defined which one
-# will be returned (and something is definitely wrong if they are not
-# identical implementations). Exception: Interset 2.0 drivers are prefered over
-# the old ones.
-#------------------------------------------------------------------------------
-sub get_driver_hash
-{
-    if(!defined($driver_hash))
-    {
-        my $driver_list = find_drivers();
-        # Index the drivers by tagset id.
-        my %hash;
-        foreach my $driver (@{$driver_list})
-        {
-            # It is possible (though not exactly desirable) that there are several drivers installed for the same tagset.
-            if(exists($hash{$driver->{tagset}}))
-            {
-                # If the previously encountered driver is old and this one is new, prefer the new one.
-                # Otherwise (both are old or both are new) just hope that the two installed modules are identical.
-                if($hash{$driver->{tagset}}{old} && !$driver->{old})
-                {
-                    $hash{$driver->{tagset}} = $driver;
-                }
-            }
-            else # this is the first driver found for this tagset
-            {
-                $hash{$driver->{tagset}} = $driver;
-            }
-        }
-        $driver_hash = \%hash;
-    }
-    return $driver_hash;
-}
-
-
-
-#------------------------------------------------------------------------------
 # Decodes a tag using a particular driver.
 #------------------------------------------------------------------------------
 =func decode()
@@ -368,6 +203,171 @@ _end_of_eval_
         confess("Did not succeed in creating driver object for '$tagset' (".ref($object)."/$object)");
     }
     return $object;
+}
+
+
+
+#------------------------------------------------------------------------------
+# Tries to enumerate existing tagset drivers. Searches for relevant folders in
+# @INC paths.
+#------------------------------------------------------------------------------
+=func find_drivers()
+
+  my $list_of_drivers = find_drivers ();
+
+This function searches relevant folders in C<@INC> for installed Interset
+drivers for tagsets.
+It looks both for the new Interset 2 drivers (e.g. C<Lingua::Interset::EN::Penn>)
+and for the old Interset 1 drivers (e.g. C<tagset::en::penn>).
+It returns a reference to an array of hash references.
+Every hash in the list contains the following fields
+(here with example values):
+
+  my %record =
+  (
+      'old'     => 1, # 1 or 0 ... old or new driver?
+      'tagset'  => 'en::penn', # tagset id
+      'package' => 'Lingua::Interset::EN::Penn', # this is what you 'use' or 'require' in your code
+      'path'    => '/home/zeman/perl5/lib/Lingua/Interset/EN/Penn.pm' # path where it is installed
+  );
+
+Note that you may find more than one package for the same tagset id.
+This function will list all of them.
+When you ask Interset to do something with a tagset (e.g. C<decode ('en::penn', $tag)>),
+Interset will select one of the available packages for you.
+It will prefer new drivers over the old ones.
+If you have two old or two new drivers, their priority will be decided by Perl
+and it should correspond to the order of your C<$PERL5LIB> environment variable.
+To avoid confusion, it is recommended that you have each package installed only once.
+
+=cut
+sub find_drivers
+{
+    if(!defined($driver_list))
+    {
+        $driver_list = _find_drivers();
+    }
+    return $driver_list;
+}
+sub _find_drivers
+{
+    my @drivers;
+    foreach my $path (@INC)
+    {
+        # Old drivers (Interset 1.0) are in the "tagset" folder.
+        # We will continue using them until all have been ported to Interset 2.0.
+        my $tpath = "$path/tagset";
+        if(-d $tpath)
+        {
+            opendir(DIR, $tpath) or confess("Cannot read folder $tpath: $!\n");
+            my @subdirs = readdir(DIR);
+            closedir(DIR);
+            foreach my $sd (@subdirs)
+            {
+                my $sdpath = "$tpath/$sd";
+                if(-d $sdpath && $sd !~ m/^\.\.?$/)
+                {
+                    opendir(DIR, $sdpath) or confess("Cannot read folder $sdpath: $!\n");
+                    my @files = readdir(DIR);
+                    closedir(DIR);
+                    foreach my $file (@files)
+                    {
+                        my $fpath = "$sdpath/$file";
+                        my $driver = $file;
+                        if(-f $fpath && $driver =~ s/\.pm$//)
+                        {
+                            $driver = $sd."::".$driver;
+                            my %record =
+                            (
+                                'old'     => 1,
+                                'tagset'  => $driver,
+                                'package' => "tagset::$driver",
+                                'path'    => $fpath
+                            );
+                            push(@drivers, \%record);
+                        }
+                    }
+                }
+            }
+        }
+        # New drivers (Interset 2.0) are in the "Lingua/Interset" folder.
+        # Not everything in this folder is a driver! But subfolders lead to drivers, the additional stuff are files, not folders.
+        my $lipath = "$path/Lingua/Interset";
+        if(-d $lipath)
+        {
+            opendir(DIR, $lipath) or confess("Cannot read folder $lipath: $!\n");
+            my @subdirs = readdir(DIR);
+            closedir(DIR);
+            foreach my $sd (@subdirs)
+            {
+                my $sdpath = "$lipath/$sd";
+                if(-d $sdpath && $sd !~ m/^\.\.?$/)
+                {
+                    opendir(DIR, $sdpath) or confess("Cannot read folder $sdpath: $!\n");
+                    my @files = readdir(DIR);
+                    closedir(DIR);
+                    foreach my $file (@files)
+                    {
+                        my $fpath = "$sdpath/$file";
+                        my $driver = $file;
+                        if(-f $fpath && $driver =~ s/\.pm$//)
+                        {
+                            my $driver_uppercased = $sd.'::'.$driver;
+                            my $driver_lowercased = lc($driver_uppercased);
+                            my %record =
+                            (
+                                'old'     => 0,
+                                'tagset'  => $driver_lowercased,
+                                'package' => "Lingua::Interset::$driver_uppercased",
+                                'path'    => $fpath
+                            );
+                            push(@drivers, \%record);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    @drivers = sort {$a->{tagset} cmp $b->{tagset}} (@drivers);
+    return \@drivers;
+}
+
+
+
+#------------------------------------------------------------------------------
+# Returns the set of all known tagset drivers indexed by tagset id. If there
+# are two drivers installed for the same tagset, it is not defined which one
+# will be returned (and something is definitely wrong if they are not
+# identical implementations). Exception: Interset 2.0 drivers are prefered over
+# the old ones.
+#------------------------------------------------------------------------------
+sub get_driver_hash
+{
+    if(!defined($driver_hash))
+    {
+        my $driver_list = find_drivers();
+        # Index the drivers by tagset id.
+        my %hash;
+        foreach my $driver (@{$driver_list})
+        {
+            # It is possible (though not exactly desirable) that there are several drivers installed for the same tagset.
+            if(exists($hash{$driver->{tagset}}))
+            {
+                # If the previously encountered driver is old and this one is new, prefer the new one.
+                # Otherwise (both are old or both are new) just hope that the two installed modules are identical.
+                if($hash{$driver->{tagset}}{old} && !$driver->{old})
+                {
+                    $hash{$driver->{tagset}} = $driver;
+                }
+            }
+            else # this is the first driver found for this tagset
+            {
+                $hash{$driver->{tagset}} = $driver;
+            }
+        }
+        $driver_hash = \%hash;
+    }
+    return $driver_hash;
 }
 
 
