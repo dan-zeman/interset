c1b8809f3e21be6e0f5a161072a1cc9bbe7ec9f3
Dan Zeman <zeman@ufal.mff.cuni.cz>
2014-11-19 11:38:46 +0100

    Converted list of known tags from the CoNLL-like format to the base list of features.

--- bin/filter_list_of_known_tags.pl
+++ bin/filter_list_of_known_tags.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/env perl
-# Načte seznam značek, dekóduje je a znova zakóduje, čímž uvede rysy do kanonického pořadí.
+# Reads the list of known tags, decodes them and encodes again. As a result, the features are in the canonical order.
+# Some features or entire tags can be filtered if desired. This is a one-time action and the exact behavior must be specified by editing this source code.
+# We typically need this script during the development of a new driver.
 # Copyright © 2014 Dan Zeman <zeman@ufal.mff.cuni.cz>
 # Licence: GNU GPL
 
@@ -8,19 +10,23 @@ use open ':utf8';
 binmode(STDIN, ':utf8');
 binmode(STDOUT, ':utf8');
 binmode(STDERR, ':utf8');
-use Lingua::Interset qw(list decode encode);
+use Lingua::Interset qw(get_driver_object);
 
-my $list = list('eu::conll');
+my $driver = get_driver_object('fi::turku');
+my $list = $driver->list();
 my %map;
 foreach my $tag (@{$list})
 {
-    my $fs = decode('eu::conll', $tag);
-    my $tag1 = encode('eu::conll', $fs);
+    #my $fs = $driver->decode($tag);
+    #my $tag1 = $driver->encode($fs);
+    # Zatím nemáme hotové metody decode() a encode(), ale chceme přepsat značky z formátu, který uměle připomíná CoNLL, do základního.
+    $tag1 = $tag;
+    $tag1 =~ s/^\S+\s+\S+\s+(\S+)$/$1/;
     $map{$tag1}++;
     # Příliš mnoho informací se ukládá do rysu other. Chceme zajistit, aby i značky, které vzniknou, když tyto informace nebudou k dispozici, byly platné.
-    $fs->set('other', '');
-    my $tag2 = encode('eu::conll', $fs);
-    $map{$tag2}++;
+    #$fs->set('other', '');
+    #my $tag2 = encode('eu::conll', $fs);
+    #$map{$tag2}++;
 }
 my @list1 = sort(keys(%map));
 foreach my $tag (@list1)
