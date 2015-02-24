979b386b8b5ab17ba48dd6aa1b53acd9ec691134
Dan Zeman <zeman@ufal.mff.cuni.cz>
2015-02-24 16:52:58 +0100

    Commit Changes and bump $VERSION

--- lib/Lingua/Interset/Tagset/EN/Penn.pm
+++ lib/Lingua/Interset/Tagset/EN/Penn.pm
@@ -7,7 +7,7 @@
 package Lingua::Interset::Tagset::EN::Penn;
 use strict;
 use warnings;
-our $VERSION = '2.039';
+our $VERSION = '2.040';
 
 use utf8;
 use open ':utf8';
--- lib/Lingua/Interset/Tagset/SV/Mamba.pm
+++ lib/Lingua/Interset/Tagset/SV/Mamba.pm
@@ -5,7 +5,7 @@
 package Lingua::Interset::Tagset::SV::Mamba;
 use strict;
 use warnings;
-our $VERSION = '2.039';
+our $VERSION = '2.040';
 
 use utf8;
 use open ':utf8';
