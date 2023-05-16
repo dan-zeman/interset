143fbaa0f533718f9fc21fcdac7c65ecce25c149
Dan Zeman <zeman@ufal.mff.cuni.cz>
2023-05-16 09:50:33 +0200

    Added comparison to the UniMorph database.

--- bin/conllu_test_uposf_unimorph.pl
+++ bin/conllu_test_uposf_unimorph.pl
@@ -10,16 +10,35 @@ use open ':utf8';
 binmode(STDIN, ':utf8');
 binmode(STDOUT, ':utf8');
 binmode(STDERR, ':utf8');
+use Getopt::Long;
 use Lingua::Interset qw(decode encode list);
 
 sub usage
 {
-    print STDERR ("Usage: $0 < input.conllu > output.conllu\n");
+    print STDERR ("Usage: $0 --unimorph path-to-file < input.conllu > output.conllu\n");
 }
+
+my $umpath;
+GetOptions
+(
+    'unimorph=s' => \$umpath
+);
 
 my %uposf2um;
 my %um2uposf;
 my %reported;
+my %stats;
+
+# Read the UniMorph file.
+my $um;
+if(defined($umpath))
+{
+    $um = read_unimorph($umpath);
+}
+else
+{
+    print STDERR ("No UniMorph file path given. The CoNLL-U data will not be checked against UniMorph database.");
+}
 
 # Read the CoNLL-U file from STDIN or from files given as arguments.
 while(<>)
@@ -54,6 +73,11 @@ while(<>)
                 }
             }
         }
+        # Compare the UniMorph features with the UniMorph database.
+        if(defined($umpath) && $f[0] =~ m/^[0-9]+$/)
+        {
+            check_unimorph($um, $f[1], $f[2], $unimorph, \%stats);
+        }
         # Replace the current XPOS tag, if any, with UniMorph.
         $f[4] = defined($unimorph) && $unimorph ne '' ? $unimorph : '_';
         $_ = join("\t", @f)."\n";
@@ -63,6 +87,12 @@ while(<>)
 }
 my $n = scalar(keys(%reported));
 print STDERR ("Total $n roundtrip conversions failed.\n");
+
+# Write the UniMorph file with frequencies.
+if(defined($umpath))
+{
+    write_unimorph($umpath.'.freq', \%hash);
+}
 
 
 
@@ -106,4 +136,109 @@ sub reset_non_unimorph_features
         }
     }
     return $fs;
+}
+
+
+
+#------------------------------------------------------------------------------
+# Reads UniMorph language file.
+#------------------------------------------------------------------------------
+sub read_unimorph
+{
+    my $path = shift; # e.g. /net/work/people/zeman/unimorph/ces/ces
+    my %hash;
+    open(UM, $path) or die("Cannot read $path: $!");
+    while(<UM>)
+    {
+        chomp();
+        # fields: lemma form features
+        my @f = split(/\t/, $_);
+        # There may be multiple analyses of the same form.
+        # Hash them by form, then by lemma, then by features.
+        $hash{$f[1]}{$f[0]}{$f[2]} = \@f;
+    }
+    close(UM);
+    return \%hash;
+}
+
+
+
+#------------------------------------------------------------------------------
+# Tests a form-lemma-features triple against the UniMorph database.
+#------------------------------------------------------------------------------
+sub check_unimorph
+{
+    my $um = shift; # hash ref
+    my $form = shift;
+    my $lemma = shift;
+    my $umfeatures = shift;
+    my $stats = shift;
+    if(defined($umfeatures) && $umfeatures ne '' && $umfeatures ne '_')
+    {
+        my $foundform;
+        if(exists($um->{$form}))
+        {
+            $stats->{n_forms_found}++;
+            $foundform = $form;
+        }
+        elsif(exists($um->{lc($form)}))
+        {
+            $stats->{n_forms_found}++;
+            $foundform = lc($form);
+        }
+        else
+        {
+            $stats->{n_forms_not_found}++;
+        }
+        if(defined($foundform))
+        {
+            if(exists($um->{$foundform}{$lemma}))
+            {
+                # The order of individual UniMorph features is not significant.
+                # Order them alphabetically before matching.
+                $umfeatures = join(';', sort(split(/;/, $umfeatures)));
+                foreach my $analysis (keys(%{$um->{$foundform}{$lemma}}))
+                {
+                    my $reordered_analysis = join(';', sort(split(/;/, $analysis)));
+                    if($reordered_analysis eq $umfeatures)
+                    {
+                        $um->{$foundform}{$lemma}{$analysis}[3]++;
+                        $stats->{n_token_analyses_found}++;
+                        last;
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+#------------------------------------------------------------------------------
+# Writes modified UniMorph language file: Only lines that were found in UD
+# data, with added column with frequencies.
+#------------------------------------------------------------------------------
+sub write_unimorph
+{
+    my $path = shift; # e.g. /net/work/people/zeman/unimorph/ces/ces
+    my $hash = shift;
+    open(UM, ">$path") or die("Cannot write $path: $!");
+    my @forms = sort(keys(%{$hash}));
+    foreach my $form (@forms)
+    {
+        my @lemmas = sort(keys(%{$hash->{$form}}));
+        foreach my $lemma (@lemmas)
+        {
+            my @analyses = sort(keys(%{$hash->{$form}{$lemma}}));
+            foreach my $analysis (@analyses)
+            {
+                my $frequency = $hash->{$form}{$lemma}{$analysis}[3];
+                if($frequency)
+                {
+                    print("$lemma\t$form\t$analysis\t$frequency\n");
+                }
+            }
+        }
+    }
+    close(UM);
 }
